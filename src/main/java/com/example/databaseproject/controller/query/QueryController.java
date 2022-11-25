3dd6cbfd16ed89c286ed2845cd75b309928adc23
minjun <mjk54565@gmail.com>
2022-11-25 21:01:16 +0900

    refactor: 계좌랑 유저 조회만 되게 변경

---
+++ src/main/java/com/example/databaseproject/controller/query/QueryController.java
@@ -0,0 +1,2 @@
+package com.example.databaseproject.controller.query;public class QueryController {
+}
---
+++ src/main/java/com/example/databaseproject/service/QueryService.java
@@ -0,0 +1,4 @@
+package com.example.databaseproject.service;
+
+public class QueryService {
+}
